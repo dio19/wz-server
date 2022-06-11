ac52350f66a77a4f3c02060139bf78e987ddedcc
dio19 <hernandezdionisiooscar@gmail.com>
2022-06-11 20:16:52 -0300

    feat: implemented error handling on requests

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 from pymongo import MongoClient
-from flask import Flask, request
+from flask import Flask, request, jsonify, abort
 from flask_pymongo import PyMongo
 import certifi
-import json
 
 ca = certifi.where()
 
@@ -13,73 +12,92 @@ app = Flask(__name__)
 app.config['MONGO_URI']=cluster
 mongo = PyMongo(app)
 
-db = client.wazuh
-users = db.users
-tasks = db.tasks
+try:
+    db = client.wazuh
+    users = db.users
+    tasks = db.tasks
+except Exception as e:
+    print('Cannot connect to DB: ', e)
 
 @app.route('/tasks', methods=['GET'])
 def data_tasks():
     collection = db.tasks.find()
-    all_tasks = []
-    for task in collection:
-        data_task = {
-            "user_id": task["user_id"],
-            "id": task["id"],
-            "title": task["title"],
-             "completed": task["completed"]
-        }
-        all_tasks.append(data_task)
-    return { "total_items": tasks.count_documents({}), "data": all_tasks }
+    if collection is not None:
+        all_tasks = []
+        for task in collection:
+            data_task = {
+                "user_id": task["user_id"],
+                "id": task["id"],
+                "title": task["title"],
+                "completed": task["completed"]
+            }
+            all_tasks.append(data_task)
+    else:
+        return jsonify({"error": "An error occurred with tasks collection"}), 503
+    try:
+        return { "total_items": tasks.count_documents({}), "data": all_tasks }
+    except Exception as e:
+        return jsonify({"An exception occurred": e})
 
 @app.route('/tasks/<int:id>', methods=['GET'])
 def data_task(id):
     task_by_id = tasks.find_one({
         "id": id
     })
-    return {
+    try:
+        return {
             "user_id": task_by_id["user_id"],
             "id": task_by_id["id"],
             "title": task_by_id["title"],
              "completed": task_by_id["completed"]
         }
+    except Exception as e:
+        return jsonify({"An exception occurred": e})
 
 @app.route('/users', methods=['GET'])
 def data_users():
     collection = db.users.find()
-    all_users = []
-    for user in collection:
-        data_user = {
-            "id": user['id'],
-            "name": user['name'],
-            "username": user['username'],
-            "email": user['email'],
-            "adress": {
-                "street": user['address']['street'],
-                "suite": user['address']['suite'],
-                "city": user['address']['city'],
-                "zipcode": user['address']['zipcode'],
-                "geo": {
-                    "lat": user['address']['geo']['lat'],
-                    "lng": user['address']['geo']['lng'],
-                }
-            },
-            "phone": user['phone'],
-            "website": user['website'],
-            "company": {
-                "name": user['company']['name'],
-                "catch_phrase": user['company']['catchPhrase'],
-                "bs": user['company']['bs']
-            },
-        }
-        all_users.append(data_user)
-    return { "total_items": users.count_documents({}), "data": all_users }
+    if collection is not None:
+        all_users = []
+        for user in collection:
+            data_user = {
+                "id": user['id'],
+                "name": user['name'],
+                "username": user['username'],
+                "email": user['email'],
+                "adress": {
+                    "street": user['address']['street'],
+                    "suite": user['address']['suite'],
+                    "city": user['address']['city'],
+                    "zipcode": user['address']['zipcode'],
+                    "geo": {
+                        "lat": user['address']['geo']['lat'],
+                        "lng": user['address']['geo']['lng'],
+                    }
+                },
+                "phone": user['phone'],
+                "website": user['website'],
+                "company": {
+                    "name": user['company']['name'],
+                    "catch_phrase": user['company']['catchPhrase'],
+                    "bs": user['company']['bs']
+                },
+            }
+            all_users.append(data_user)
+    else:
+        return jsonify({"error": "An error occurred with users collection"}), 503
+    try:
+        return { "total_items": users.count_documents({}), "data": all_users }
+    except Exception as e:
+        return jsonify({"An exception occurred": e})
 
 @app.route('/users/<int:user_id>', methods=['GET'])
 def data_user(user_id):
     user_by_id = users.find_one({
         "id": user_id
     })
-    return {
+    try:
+        return {
             "id": user_by_id['id'],
             "name": user_by_id['name'],
             "username": user_by_id['username'],
@@ -102,17 +120,20 @@ def data_user(user_id):
                 "bs": user_by_id['company']['bs']
             },
         }
+    except Exception as e:
+        return jsonify({"An exception occurred": e})
 
 @app.route('/users/<int:user_id>/tasks', methods=['GET'])
 def data_tasks_by_user(user_id):
     completed = request.args.get("completed", type=str)
-    title = request.args.get("title", default="", type=str)
     def t_or_f(arg):
         ua = str(arg).upper()
         if 'TRUE'.startswith(ua):
             return True
         elif 'FALSE'.startswith(ua):
             return False
+        else:
+            return abort(422, description="Please enter a boolean value, true or false")
     tasks_by_user = []
     if completed is not None:
         tasks_by_user = tasks.find({
@@ -129,21 +150,16 @@ def data_tasks_by_user(user_id):
             "user_id": task["user_id"],
             "id": task["id"],
             "title": task["title"],
-             "completed": task["completed"]
+            "completed": task["completed"]
         }
         data.append(data_task)
-        def iterator_func(x):
-            if title in x["title"]:
-                print("Exists")
-                return x
-            else:
-                print("Does not exist")
-    filtered_by_title = filter(iterator_func, data)
-    print(list(filtered_by_title))
-    return {
-        "total_items": len(data),
-        "data": data
-    }
+    try:
+        return {
+            "total_items": len(data),
+            "data": data
+        }
+    except Exception as e:
+        return jsonify({"An exception occurred": e})
 
 if __name__ == "__main__":
     app.run(debug=True)
